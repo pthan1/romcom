a6be42e6bf9f97f3be0229002d67d8f7b3d54635
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-07-16 16:33:46 -0600

    grab form inputs to create custom book

--- src/main.js
+++ src/main.js
@@ -18,6 +18,12 @@ let saveBtn = document.querySelector('.save-cover-button');
 let savedViewBtn = document.querySelector('.view-saved-button');
 let makeViewBtn = document.querySelector('.make-new-button');
 
+let coverField = document.getElementById('cover');
+let titleField = document.getElementById('title');
+let descriptor1Field = document.getElementById('descriptor1');
+let descriptor2Field = document.getElementById('descriptor2');
+let createBookBtn = document.querySelector('.create-new-book-button');
+
 const savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -27,6 +33,7 @@ randBtn.addEventListener('click', changeCover);
 // saveBtn
 savedViewBtn.addEventListener('click', showSavedView);
 makeViewBtn.addEventListener('click', showMakeView);
+createBookBtn.addEventListener('click', createBook);
 
 function changeCover() {
   newCover = new Cover(covers[getRandIndex(covers)], titles[getRandIndex(titles)], descriptors[getRandIndex(descriptors)], descriptors[getRandIndex(descriptors)]);
@@ -61,6 +68,22 @@ function showMakeView() {
   saveBtn.classList.add('hidden');
   homeView.classList.add('hidden');
   makeView.classList.remove('hidden');
+}
+
+function createBook() {
+  console.log(covers, titles, descriptors)
+  console.log('before', coverField.value)
+  covers.push(coverField.value);
+  titles.push(titleField.value);
+  descriptors.push(descriptor1Field.value, descriptor2Field.value);
+  console.log(covers, titles, descriptors)
+  console.log('after', coverField.value)
+
+  customBook = new Cover(coverField.value, titleField.value, descriptor1Field.value, descriptor2Field.value);
+  cover.src = customBook.cover;
+  title.innerText = customBook.title;
+  tagline1.innerText = customBook.tagline1;
+  tagline2.innerText = customBook.tagline2;
 }
 
 function getRandIndex(array) {
