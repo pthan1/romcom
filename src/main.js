b9e874c65f819542beacd5aea91e1c3bfbc1f356
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-07-17 10:57:15 -0700

    Modify showSaved to retain added covers

--- src/main.js
+++ src/main.js
@@ -65,16 +65,22 @@ function showSaved() {
   savedView.classList.remove('hidden');
   makeView.classList.add('hidden');
 
+  savedSection.innerHTML = "";
+
   for (let i = 0; i < savedCovers.length; i++) {
-    savedSection.innerHTML = `
-      <section class="mini-cover">
+    // if (currentCover.id === savedCovers[i].id) {
+    //   return;
+    // }
+
+    savedSection.insertAdjacentHTML('beforeend', `
+      <span class="mini-cover">
         <img class="cover-image mini-cover" src=${savedCovers[i].cover}>
         <h2 class="cover-title">${savedCovers[i].title}</h2>
         <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
         <img class="price-tag" src="./assets/price.png">
         <img class="overlay" src="./assets/overlay.png">
-      </section>
-    `;
+      </span>
+    `);
   }
 }
 
